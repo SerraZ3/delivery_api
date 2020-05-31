40ffd0ebf16fa0a9b50bffda2f7d5a142ec39035
SerraZ3 <serra.henrique3@gmail.com>
2020-05-31 18:25:50 -0300

    :wolf: Created migration for order product

---
+++ database/migrations/0000000006100_order_product_schema.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class OrderProductSchema extends Schema {
+  up() {
+    this.create("order_products", (table) => {
+      table.increments();
+      table.integer("quantity").unsigned();
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("order_products");
+  }
+}
+
+module.exports = OrderProductSchema;
