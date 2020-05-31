b4d8e9363c1150d939f36b60982b6c2fbb22148b
SerraZ3 <serra.henrique3@gmail.com>
2020-05-31 19:36:01 -0300

    :wolf: Created migration for discount application

---
+++ database/migrations/0000000009000_discount_application_schema.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class DiscountApplicationSchema extends Schema {
+  up() {
+    this.create("discount_applications", (table) => {
+      table.increments();
+      table.string("name");
+      table.string("slug", 5);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("discount_applications");
+  }
+}
+
+module.exports = DiscountApplicationSchema;
