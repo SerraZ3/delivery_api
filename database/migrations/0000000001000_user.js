16f9f3774a8db03d798a8c9c04cd62d9d89a385f
SerraZ3 <serra.henrique3@gmail.com>
2020-05-31 17:35:49 -0300

    :wolf: Changed user migration

--- database/migrations/0000000001000_user.js
+++ database/migrations/0000000001000_user.js
@@ -7,9 +7,10 @@ class UserSchema extends Schema {
   up() {
     this.create("users", (table) => {
       table.increments();
-      table.string("username", 80).notNullable().unique();
       table.string("email", 254).notNullable().unique();
       table.string("password", 60).notNullable();
+      table.boolean("active").default(0);
+      table.timestamp("email_verified_at").nullable();
       table.timestamps();
     });
   }
