059b7e7696486af2a388d541bf03b699f6c5ff5b
SerraZ3 <serra.henrique3@gmail.com>
2020-06-01 02:59:11 -0300

    :wolf: Created model for loyalty card user

---
+++ app/Models/LoyaltyCardUser.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use("Model");
+
+class LoyaltyCardUser extends Model {}
+
+module.exports = LoyaltyCardUser;
