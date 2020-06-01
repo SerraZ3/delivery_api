ccba6b5f23f8527d59583c82f3751e39b001bae2
SerraZ3 <serra.henrique3@gmail.com>
2020-06-01 03:21:49 -0300

    :wolf: Created relationship model between coupon and discount application. 1 to N

--- app/Models/Coupon.js
+++ app/Models/Coupon.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 
-class Coupon extends Model {}
+class Coupon extends Model {
+  discountApplication() {
+    return this.belongsTo("App/Models/DiscountApplication");
+  }
+}
 
 module.exports = Coupon;
--- app/Models/DiscountApplication.js
+++ app/Models/DiscountApplication.js
@@ -7,6 +7,9 @@ class DiscountApplication extends Model {
   loyaltyCards() {
     return this.hasMany("App/Models/LoyaltyCard");
   }
+  coupons() {
+    return this.hasMany("App/Models/Coupon");
+  }
 }
 
 module.exports = DiscountApplication;
