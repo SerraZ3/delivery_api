19234d968591424d08e9c0c9096456e38b8b62b5
SerraZ3 <serra.henrique3@gmail.com>
2020-05-31 22:40:36 -0300

    :wolf: Created relationship between product category and image. N to N Relationship

---
+++ database/migrations/2000000003000_product_category_image_schema.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class ProductCategoryImageSchema extends Schema {
+  up() {
+    this.create("product_category_images", (table) => {
+      table
+        .integer("image_id")
+        .unsigned()
+        .references("id")
+        .inTable("images")
+        .onDelete("CASCADE");
+      table
+        .integer("product_category_id")
+        .unsigned()
+        .references("id")
+        .inTable("product_categories")
+        .onDelete("CASCADE");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("product_category_images");
+  }
+}
+
+module.exports = ProductCategoryImageSchema;
