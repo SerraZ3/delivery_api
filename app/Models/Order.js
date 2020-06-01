38bba8044d53d8a56da43c5bb358fa280662bc7d
SerraZ3 <serra.henrique3@gmail.com>
2020-06-01 02:25:14 -0300

    :wolf: Created relationship model between order and order status. 1 to N

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 
-class Order extends Model {}
+class Order extends Model {
+  orderStatus() {
+    return this.belongsTo("App/Models/OrderStatus");
+  }
+}
 
 module.exports = Order;
--- app/Models/OrderStatus.js
+++ app/Models/OrderStatus.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 
-class OrderStatus extends Model {}
+class OrderStatus extends Model {
+  orders() {
+    return this.hasMany("App/Models/Order");
+  }
+}
 
 module.exports = OrderStatus;
