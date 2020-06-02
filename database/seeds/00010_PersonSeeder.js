c033b26e7b50bc35070c06d35572e319a12f1952
SerraZ3 <serra.henrique3@gmail.com>
2020-06-01 22:25:17 -0300

    :wolf: Created seed for person

---
+++ database/seeds/00010_PersonSeeder.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| PersonSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+
+class PersonSeeder {
+  async run() {
+    let people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    await Promise.all(
+      await people.map(async (num) => {
+        await Factory.model("App/Models/Person").create({
+          user_id: num,
+          address_id: num
+        });
+      })
+    );
+  }
+}
+
+module.exports = PersonSeeder;
