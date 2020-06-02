d611e7b74f7bd67cda45a09079958a0dd3e63c93
SerraZ3 <serra.henrique3@gmail.com>
2020-06-02 13:24:49 -0300

    :wolf: Created route to auth

---
+++ start/routes/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+// Importa roda de autenticação
+require("./auth");
+// // Importa roda de admin
+// require("./admin");
+// // Importa roda de admin
+// require("./client");
