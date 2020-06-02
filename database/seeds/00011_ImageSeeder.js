f4d46b9e308486e25beaae6999a95f08f86a578b
SerraZ3 <serra.henrique3@gmail.com>
2020-06-01 23:07:20 -0300

    :wolf: Created seed for images

---
+++ database/seeds/00011_ImageSeeder.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| ImageSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+
+class ImageSeeder {
+  async run() {
+    await Factory.model("App/Models/Image").createMany(50);
+  }
+}
+
+module.exports = ImageSeeder;
