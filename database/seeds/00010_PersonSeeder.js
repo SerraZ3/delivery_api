2e76572a8739a9429c03766592727af905d58714
SerraZ3 <serra.henrique3@gmail.com>
2020-06-02 01:24:21 -0300

    :wolf: Added more person

--- database/seeds/00010_PersonSeeder.js
+++ database/seeds/00010_PersonSeeder.js
@@ -15,7 +15,7 @@ const Factory = use("Factory");
 
 class PersonSeeder {
   async run() {
-    let people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     await Promise.all(
       await people.map(async (num) => {
         await Factory.model("App/Models/Person").create({
