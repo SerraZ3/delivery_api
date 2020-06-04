3d7be497a47496557a95ba6faa8bfa81e5b5ec0d
SerraZ3 <serra.henrique3@gmail.com>
2020-06-04 02:17:29 -0300

    :wolf: Changed active user from false to true

--- database/migrations/0000000001000_user.js
+++ database/migrations/0000000001000_user.js
@@ -9,7 +9,7 @@ class UserSchema extends Schema {
       table.increments();
       table.string("email", 254).notNullable().unique();
       table.string("password", 60).notNullable();
-      table.boolean("active").default(0);
+      table.boolean("active").default(1);
       table.timestamp("email_verified_at").nullable();
       table.timestamps();
     });
