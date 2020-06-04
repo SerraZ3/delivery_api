12d0771fc40a39f21eedb7d55d81aec2594d3be3
SerraZ3 <serra.henrique3@gmail.com>
2020-06-04 03:21:28 -0300

    :wolf: Added computed value to url image

--- app/Models/Image.js
+++ app/Models/Image.js
@@ -2,8 +2,15 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
+const Env = use("Env");
 
 class Image extends Model {
+  static get computed() {
+    return ["url"];
+  }
+  getUrl({ path }) {
+    return `${Env.get("APP_URL")}/uploads/${path}`;
+  }
   products() {
     return this.belongsToMany("App/Models/Product").pivotModel(
       "App/Models/ProductImage"
