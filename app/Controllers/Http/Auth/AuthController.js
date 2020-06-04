e07c82d0feb094fc6d019e1f342dc6157e1017f1
SerraZ3 <serra.henrique3@gmail.com>
2020-06-03 23:49:37 -0300

    :wolf: Added more validator for register and clear code

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -20,85 +20,48 @@ class AuthController {
     try {
       const { email, password, password_confirmation, person } = request.all();
 
-      // Checa se as senhas coincidem
-      if (password !== password_confirmation) {
-        throw {
-          code: 1000,
-          message: "Senhas não coincidem"
-        };
-      }
-      // Verifica se email existe
-      const checkEmail = await User.findBy("email", email);
-      if (checkEmail) {
-        throw {
-          code: 1001,
-          message: "Email já cadastrado!"
-        };
-      }
-
       // Cria usuário
       const user = await User.create({ email, password }, trx);
 
       // Verifica se os dados da pessoa existe para ser registrado
       if (person) {
-        if (person.name) {
-          if (person.cpf) {
-            if (person.date_birth) {
-              // ===========================
-              //  Validation for date birth
-              // ===========================
+        // ===========================
+        //  Validation for date birth
+        // ===========================
 
-              // Verifica através de regex o formato da data de nascimento
-              let dateValid = person.date_birth.match(
-                /(\d{2})\/(\d{2})\/(\d{4})/,
-                "$1/$2/$3"
-              );
-              if (dateValid === null) {
-                throw {
-                  code: 1003,
-                  message: "Formato da data inválido. Formato ex.: dd/mm/yyyy"
-                };
-              }
-              // Transform date_birth in timestamp
-              person.date_birth = new Date(
-                person.date_birth.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")
-              );
-
-              // ====================
-              //  Validation for cpf
-              // ====================
-
-              // Remove characters especiais
-              person.cpf = person.cpf.replace(/[^0-9 ]/g, "");
-
-              // Valida o cpf
-              if (!cpf.isValid(person.cpf)) {
-                throw {
-                  code: 1004,
-                  message: "CPF inválido"
-                };
-              }
-
-              // Cria a pessoa vinculando as informações do usuário
-              await user.person().create(person, trx);
-            } else {
-              throw {
-                code: 1005,
-                message: "Preencha com a data de nascimento"
-              };
-            }
-          } else {
-            throw {
-              code: 1006,
-              message: "Preencha com o CPF"
-            };
-          }
-        } else {
+        // Verifica através de regex o formato da data de nascimento
+        let dateValid = person.date_birth.match(
+          /(\d{2})\/(\d{2})\/(\d{4})/,
+          "$1/$2/$3"
+        );
+        if (dateValid === null) {
           throw {
-            code: 1007,
-            message: "Preencha com o nome"
+            code: 1003,
+            message: "Formato da data inválido. Formato ex.: dd/mm/yyyy"
           };
         }
+        // Transform date_birth in timestamp
+        person.date_birth = new Date(
+          person.date_birth.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")
+        );
+
+        // ====================
+        //  Validation for cpf
+        // ====================
+
+        // Remove characters especiais
+        person.cpf = person.cpf.replace(/[^0-9 ]/g, "");
+
+        // Valida o cpf
+        if (!cpf.isValid(person.cpf)) {
+          throw {
+            code: 1004,
+            message: "CPF inválido"
+          };
+        }
+
+        // Cria a pessoa vinculando as informações do usuário
+        await user.person().create(person, trx);
       }
 
       const userRole = await Role.findBy("slug", "client");
--- app/Validators/Auth/Login.js
+++ app/Validators/Auth/Login.js
@@ -4,12 +4,12 @@ class AuthLogin {
   get rules() {
     return {
       // validation rules
-      email: "required|email|unique:users",
-      password: "required",
-      password_confirmation: "required",
-      "person.name": "requiredIf:person",
-      "person.cpf": "requiredIf:person",
-      "person.date_birth": "requiredIf:person"
+      email: "required|email|unique:users|string",
+      password: "required|confirmed|string",
+      password_confirmation: "required|string",
+      "person.name": "requiredIf:person|string",
+      "person.cpf": "requiredIf:person|string",
+      "person.date_birth": "requiredIf:person|string"
     };
   }
   // Mostra todos os erros ao inves de apenas um
@@ -19,9 +19,11 @@ class AuthLogin {
   // Personaliza a mensagem de validação
   get messages() {
     return {
+      string: "O campo '{{field}}' deve ser uma string",
       "email.required": "O e-mail é obrigatório",
       "email.email": "O e-mail inválido",
       "email.unique": "O e-mail já está cadastrado",
+      confirmed: "As senhas não coincidem",
       "password.required": "A senha é obrigatório",
       "password_confirmation.required": "A confirmação da senha é obrigatório",
       "person.name.requiredIf": "O nome da pessoa é obrigatório",
