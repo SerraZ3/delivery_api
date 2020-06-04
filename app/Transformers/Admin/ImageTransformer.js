07774f11fd04e40506d7b802f1b94275db91d081
SerraZ3 <serra.henrique3@gmail.com>
2020-06-04 03:40:20 -0300

    :wolf: Created image admin transformer

--- app/Transformers/Admin/ImageTransformer.js
+++ app/Transformers/Admin/ImageTransformer.js
@@ -12,12 +12,15 @@ class ImageTransformer extends BumblebeeTransformer {
   /**
    * This method is used to transform the data.
    */
-  transform(model) {
+  transform(image) {
     image = image.toJSON();
     return {
       // add your transformation object here
       id: image.id,
-      url: image.url
+      url: image.url,
+      size: image.size,
+      original_name: image.original_name,
+      extension: image.extension
     };
   }
 }
