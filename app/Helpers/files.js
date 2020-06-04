14be2a95af98520e95371a2800cc8f80778dd76b
SerraZ3 <serra.henrique3@gmail.com>
2020-06-04 02:24:50 -0300

    :wolf: Created files helper

---
+++ app/Helpers/files.js
@@ -0,0 +1,55 @@
+const Helpers = use("Helpers");
+/**
+ * Move um unico arquivo para o caminho especificado, se nenhum for especificado entao o public/uploads será utilizado
+ *
+ * @param {FileJar} file arquvio a ser gerenciado
+ * @param {string} path caminho onde o arquvio deve ser movido
+ * @return {Object<fileJar>}
+ */
+const manage_single_upload = async (file, path = null) => {
+  path ? path : Helpers.publicPath("uploads");
+
+  // Gera nome aleatorio
+  const random_name = await str_random(30);
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtpe}`;
+
+  // Renomeia o arquivo e move ele para o path
+  await file.move(path, { name: filename });
+
+  return file;
+};
+
+/***
+ * Move um mulitplos arquivo para o caminho especificado, se nenhum for especificado entao o public/uploads será utilizado
+ * @param {FileJar} fileJar arquvio a ser gerenciado
+ * @param {string} path caminho onde o arquvio deve ser movido
+ */
+const manage_multiple_upload = async (file, path = null) => {
+  path ? path : Helpers.publicPath("uploads");
+
+  let successes = [],
+    errors = [];
+
+  await Promise.all(
+    fileJar.files.map(async (file) => {
+      let random_name = await str_random(30);
+      let filename = `${new Date().getTime()}-${random_name}.${file.subtpe}`;
+
+      // Renomeia o arquivo e move ele para o path
+      await file.move(path, { name: filename });
+
+      if (file.moved()) {
+        successes.push(file);
+      } else {
+        errors.push(file);
+      }
+    })
+  );
+
+  return { successes, errors };
+};
+
+module.exports = {
+  manage_single_upload,
+  manage_multiple_upload
+};
