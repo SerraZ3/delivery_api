eb5891e287fafe110b7da856be411ec923e8c7b5
SerraZ3 <serra.henrique3@gmail.com>
2020-06-09 02:38:46 -0300

    :wolf: Created controller for order status

--- app/Controllers/Http/Admin/OrderStatusController.js
+++ app/Controllers/Http/Admin/OrderStatusController.js
@@ -1,8 +1,8 @@
 "use strict";
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const OrderStatus = use("App/Models/OrderStatus");
+const Transform = use("App/Transformers/Admin/OrderStatusTransformer");
+const Database = use("Database");
 
 /**
  * Resourceful controller for interacting with orderstatuses
@@ -15,30 +15,25 @@ class OrderStatusController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {Transform} ctx.transform
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination, transform }) {
+    const name = request.input("name");
 
-  /**
-   * Render a form to be used for creating a new orderstatus.
-   * GET orderstatuses/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create({ request, response, view }) {}
+    const query = OrderStatus.query();
 
-  /**
-   * Create/save a new orderstatus.
-   * POST orderstatuses
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store({ request, response }) {}
+    if (name) {
+      // LIKE  = Case sitive
+      // ILIKE = Not Case sitive
+      query.where("name", "ILIKE", `%${name}%`);
+      query.orWhere("slug", "ILIKE", `%${name}%`);
+    }
+
+    let orderStatus = await query.paginate(pagination.page, pagination.limit);
+    orderStatus = await transform.paginate(orderStatus, Transform);
+
+    return response.send(orderStatus);
+  }
 
   /**
    * Display a single orderstatus.
@@ -49,18 +44,66 @@ class OrderStatusController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, transform, response }) {
+    let orderStatus = await OrderStatus.findOrFail(id);
+
+    orderStatus = await transform.item(
+      orderStatus,
+      "Admin/OrderStatusTransformer.withTimestamp"
+    );
+    return response.send(orderStatus);
+  }
 
   /**
-   * Render a form to update an existing orderstatus.
-   * GET orderstatuses/:id/edit
+   * Create/save a new orderstatus.
+   * POST orderstatuses
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async edit({ params, request, response, view }) {}
+  async store({ request, response, transform }) {
+    const trx = await Database.beginTransaction();
+
+    try {
+      const { name, slug, color } = request.all();
+
+      if (color.match(/([#]{1}[0-9a-fA-f]{5,8})/g) === null) {
+        throw {
+          error: {
+            message: "Formato da cor inválido",
+            field: "color"
+          }
+        };
+      }
+
+      let orderStatus = await OrderStatus.create(
+        {
+          name,
+          slug,
+          color
+        },
+        trx
+      );
+
+      await trx.commit();
+
+      orderStatus = await transform.item(orderStatus, Transform);
+
+      return response.status(201).send(orderStatus);
+    } catch (error) {
+      await trx.rollback();
+
+      response.status(400).send(
+        error.error
+          ? error
+          : {
+              message:
+                "Não foi possivel criar o status do pedido nesse momento!"
+            }
+      );
+    }
+  }
 
   /**
    * Update orderstatus details.
@@ -70,7 +113,41 @@ class OrderStatusController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response, transform }) {
+    const trx = await Database.beginTransaction();
+    let orderStatus = await OrderStatus.findOrFail(id);
+    try {
+      const { name, slug, color } = request.all();
+      if (color.match(/([#]{1}[0-9a-fA-f]{5,8})/g) === null) {
+        throw {
+          error: {
+            message: "Formato da cor inválido",
+            field: "color"
+          }
+        };
+      }
+
+      orderStatus.merge({ name, slug, color });
+      await orderStatus.save(trx);
+
+      await trx.commit();
+
+      orderStatus = await transform.item(orderStatus, Transform);
+
+      return response.status(201).send(orderStatus);
+    } catch (error) {
+      await trx.rollback();
+
+      response.status(400).send(
+        error.error
+          ? error
+          : {
+              message:
+                "Não foi possivel atualizar o status do pedido nesse momento!"
+            }
+      );
+    }
+  }
 
   /**
    * Delete a orderstatus with id.
@@ -80,7 +157,20 @@ class OrderStatusController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, response }) {
+    const trx = await Database.beginTransaction();
+    const orderStatus = await OrderStatus.findOrFail(id);
+    try {
+      await orderStatus.delete(trx);
+      trx.commit();
+      return response.status(204).send();
+    } catch (error) {
+      trx.rolback();
+      return response
+        .status(500)
+        .send({ message: "Não foi possivel deletar esse status de pedido" });
+    }
+  }
 }
 
 module.exports = OrderStatusController;
