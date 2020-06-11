fb42917c14fc9e054b32ae96cb063edd08ceeb4d
SerraZ3 <serra.henrique3@gmail.com>
2020-06-10 22:13:11 -0300

    :wolf: Created validators for order status update

---
+++ app/Validators/Admin/OrderStatusUpdate.js
@@ -0,0 +1,27 @@
+"use strict";
+const { rule } = use("Validator");
+
+class AdminOrderStatusStore {
+  get rules() {
+    return {
+      // validation rules
+      name: "string",
+      slug: "string",
+      color: "string"
+    };
+  }
+  // Mostra todos os erros ao inves de apenas um
+  get validateAll() {
+    return true;
+  }
+  // Personaliza a mensagem de validação
+  get messages() {
+    return {
+      array: "O campo '{{field}}' deve ser um array",
+      string: "O campo '{{field}}' deve ser uma string",
+      required: "O campo '{{field}}' é obrigatório"
+    };
+  }
+}
+
+module.exports = AdminOrderStatusStore;
