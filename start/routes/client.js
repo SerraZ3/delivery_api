c11c616db94973dee7ef8742f0c51e19ebec4af7
SerraZ3 <serra.henrique3@gmail.com>
2020-06-17 09:57:31 -0300

    :wolf: Created route for products, product categories, user and orders

--- start/routes/client.js
+++ start/routes/client.js
@@ -9,8 +9,14 @@ Route.group(() => {
    *
    * */
   Route.resource("products", "Admin/ProductController").only(["index", "show"]);
+
   Route.get("product-categories", "Client/ProductController.categoryList");
+
   Route.resource("user", "Admin/UserController")
-    .only(["show", "update", "delete"])
+    .only(["show", "update", "destroy"])
     .middleware(["auth:jwt"]);
+
+  Route.resource("orders", "Client/OrderController")
+    .only(["show", "store", "destroy", "update"])
+    .middleware(["auth:jwt", "is:client"]);
 }).prefix("v1/api/client");
