d6d06a9213311e4934294be4d5bbe170be37c0fe
SerraZ3 <serra.henrique3@gmail.com>
2020-06-23 18:46:17 -0300

    :wolf: Created pivot between establishment_user and permission

---
+++ database/migrations/2000000015000_establishment_user_permission_schema.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class EstablishmentUserPermissionSchema extends Schema {
+  up() {
+    this.create("establishment_user_permission", (table) => {
+      table.increments();
+      table
+        .integer("establishment_user_id")
+        .unsigned()
+        .references("id")
+        .inTable("establishment_user")
+        .onDelete("CASCADE");
+      table
+        .integer("permission_id")
+        .unsigned()
+        .references("id")
+        .inTable("permissions")
+        .onDelete("CASCADE");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("establishment_user_permission");
+  }
+}
+
+module.exports = EstablishmentUserPermissionSchema;
