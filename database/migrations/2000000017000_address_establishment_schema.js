a938fb7b6b0842e61a852c86dd85928b3ae3cbea
SerraZ3 <serra.henrique3@gmail.com>
2020-06-23 18:46:57 -0300

    :wolf: Created pivot between address and establishment

---
+++ database/migrations/2000000017000_address_establishment_schema.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class AddressEstablishmentSchema extends Schema {
+  up() {
+    this.create("address_establishments", (table) => {
+      table.increments();
+      table
+        .integer("establishment_id")
+        .unsigned()
+        .references("id")
+        .inTable("establishments")
+        .onDelete("CASCADE");
+      table
+        .integer("address_id")
+        .unsigned()
+        .references("id")
+        .inTable("addresses")
+        .onDelete("CASCADE");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("address_establishments");
+  }
+}
+
+module.exports = AddressEstablishmentSchema;
