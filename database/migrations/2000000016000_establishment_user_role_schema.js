fc6c490ff348c9261b6bcaef80e9ca161470133d
SerraZ3 <serra.henrique3@gmail.com>
2020-06-23 18:46:30 -0300

    :wolf: Created pivot between establishment_user and role

---
+++ database/migrations/2000000016000_establishment_user_role_schema.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class EstablishmentUserRoleSchema extends Schema {
+  up() {
+    this.create("establishment_user_role", (table) => {
+      table.increments();
+      table
+        .integer("establishment_user_id")
+        .unsigned()
+        .references("id")
+        .inTable("establishment_user")
+        .onDelete("CASCADE");
+      table
+        .integer("role_id")
+        .unsigned()
+        .references("id")
+        .inTable("roles")
+        .onDelete("CASCADE");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("establishment_user_role");
+  }
+}
+
+module.exports = EstablishmentUserRoleSchema;
