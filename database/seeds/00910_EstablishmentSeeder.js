56ebc189e50049f382760215d31b2fecfb3e855e
SerraZ3 <serra.henrique3@gmail.com>
2020-06-23 22:52:37 -0300

    :wolf: Added user to establishmente

--- database/seeds/00910_EstablishmentSeeder.js
+++ database/seeds/00910_EstablishmentSeeder.js
@@ -13,6 +13,7 @@
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use("Factory");
 const EstablishmentUser = use("App/Models/EstablishmentUser");
+const User = use("App/Models/User");
 
 class EstablishmentSeeder {
   async run() {
@@ -26,7 +27,23 @@ class EstablishmentSeeder {
       phone: phone.id
     });
 
-    await establishment.users().attach([1, 2, 3]);
+    let userAdmin = await User.findBy("email", "serra.henrique3@gmail.com");
+    let userManager = await User.findBy("email", "serra.henrique1@gmail.com");
+    let deliveryman = await User.findBy("email", "luiz1@gmail.com");
+
+    await establishment
+      .users()
+      .attach([userAdmin.id, userManager.id, deliveryman.id]);
+
+    await (await EstablishmentUser.findBy("user_id", userAdmin.id))
+      .roles()
+      .attach([1]);
+    await (await EstablishmentUser.findBy("user_id", userManager.id))
+      .roles()
+      .attach([2]);
+    await (await EstablishmentUser.findBy("user_id", deliveryman.id))
+      .roles()
+      .attach([4]);
   }
 }
 
